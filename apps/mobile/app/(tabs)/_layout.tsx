04bfb113b53c289bf344e1c46101c5fb67d75aef
Felix Thape <67233923+DasProffi@users.noreply.github.com>
2026-09-23 15:32:21 +0200

    fix: fix app routing

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -12,42 +12,6 @@ export default function TabsLayout() {
       <Tabs.Screen name="index" />
       <Tabs.Screen name="chat" />
       <Tabs.Screen name="profile" />
-      <Tabs.Screen
-        name="doctors"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="doctor-search"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="doctor/[id]"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="personal-information"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="medications"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="requests"
-        options={{
-          href: null,
-        }}
-      />
     </Tabs>
   )
 }
